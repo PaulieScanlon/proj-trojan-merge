0bb94b0edd425f824490223f09b326fa0029b4cb
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-31 11:57:32 +0100

    feat: add read diff

--- schema.sql
+++ schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE users (
   email       VARCHAR(255) NOT NULL UNIQUE,
   country     VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE migrations (
+   migration_id    SERIAL PRIMARY KEY,  
+   migration_name  VARCHAR(255) NOT NULL, 
+   applied_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+   status          VARCHAR(50) NOT NULL,  
+   environment     VARCHAR(50) NOT NULL  
+);
