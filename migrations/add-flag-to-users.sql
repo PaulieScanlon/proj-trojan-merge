1e5caca1932edbc467dd07f6f10f204e25e335e6
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-31 16:45:09 +0100

    chore: add migrations

--- migrations/add-flag-to-users.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-ALTER TABLE users
-ADD COLUMN flag VARCHAR(255);
-
-INSERT INTO migrations (migration_name, applied_at, status, environment)
-VALUES ('add_user_flag', NOW(), 'Completed', 'production');
-
-COMMIT;
